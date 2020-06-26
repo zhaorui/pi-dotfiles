50b948ae6dbeab53ce4bfc3f0846bb43a163ba65
zhaorui <zhaoruiexplorer@icloud.com>
2020-06-26 09:37:31 +0800

    install oh-my-zsh for non-privileged user

---
+++ basic.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+setup_color() {
+	# Only use colors if connected to a terminal
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		BOLD=""
+		RESET=""
+	fi
+}
+
+setup_dns() {
+    sudo cp /etc/systemd/resolved.conf /etc/systemd/resolved.conf.bak
+    sudo sh -c 'echo "DNS=8.8.8.8 114.114.114.114" >> /etc/systemd/resolved.conf'
+    sudo systemctl restart systemd-resolved
+}
+
+reset_dns() {
+    sudo cp /etc/systemd/resolved.conf.bak /etc/systemd/resolved.conf
+    sudo systemctl restart systemd-resolved
+}
+
--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+source ./basic.sh
+
+groups | egrep -qw "root|admin|sudo" || eval 'printf "Need privileges to run\n"; exit 1'
+
 mkdir -p ~/Code
 mkdir -p ~/Scripts
-
 
 sudo apt update
 sudo apt upgrade
@@ -14,8 +17,12 @@ git clone --depth=1 https://github.com/zhaorui/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_basic_vimrc.sh
 
 # setup zsh
+export ZSH=
+rm -rf ~/.oh-my-zsh/
+setup_dns
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ./add_zsh_plugin.py zsh-autosuggestions
+reset_dns
 
